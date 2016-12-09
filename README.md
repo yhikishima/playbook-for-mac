2f1486af5a831aa44b0f405cef53405bcebf7845
yhikishima <lallapalooza1120@gmail.com>
2016-12-09 22:39:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Playbook-for-mac
+Macの環境構築時に実行する
+
+## 実行手順
+
+```
+# シェルを実行（Dry run）
+$ sh exe.sh test
+
+# シェルを実行（実際の実行）
+$ sh exe.sh do
+```
---
+++ exe.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Xcodeのインストール
+# Homebrewのインストール
+# Ansibleのインストール
+# gitのインストール
+# Homebrew Caskオプションの設定
+# Playbookの実行
+
+if [ $1 -eq "test" ]; then
+  echo "Dry Runの実行"
+  ansible-playbook -vv localhost.yml -i hosts -C
+elif [ $1 -eq "do" ]; then
+  echo "Xcodeのインストール&"
+  echo "Homebrewのインストール&"
+  echo "Ansibleのインストール&"
+  echo "gitのインストール&"
+  echo "Homebrew Caskオプションの設定&"
+  echo "Playbookの実行をします"
+  sudo xcodebuild -license&&ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"&&brew update&&brew install ansible&&echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile&&source ~/.bash_profile&&ansible-playbook -vv localhost.yml -i hosts
+fi
